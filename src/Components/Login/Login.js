ad4adc0e2e2f99c02e257060be2042a9ef9d98be
raju120405 <raju120405@gmail.com>
2022-05-04 23:30:26 +0600

    firebase working

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import auth from '../../Firebase/Firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    const hanleSignIn = ()=>{
+        signInWithGoogle()
+    }
     return (
         <div>
             <h2>this is login pages</h2>
-           <div className='text-center '> <button type="button" class="btn btn-success">Google Sign In</button></div>
+           <div className='text-center '> <button onClick={hanleSignIn} type="button" class="btn btn-success">Google Sign In</button></div>
         </div>
     );
 };
