0df138061a180d4f87597f44d877c3851f484d13
raju120405 <raju120405@gmail.com>
2022-05-06 16:42:48 +0600

    product upload

--- src/Components/UpdateProduct/UpdateProduct.js
+++ src/Components/UpdateProduct/UpdateProduct.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
 const UpdateProduct = () => {
+    const handelupload = (event)=>{
+        event.preventDefault();
+
+        const name = event.target.name.value;
+        const price = event.target.price.value;
+        console.log(name,price)
+    }
     return (
         <div>
             <h2 className='text-center mt-3 text-primary'>Upload your Product</h2>
             <div className='w-50 mx-auto'>
-                <form>
+                <form onSubmit={handelupload}>
                     <div class="mb-3 ">
                         <label  class="form-label">Product name</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <input type="text" name='name' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                         
                     </div>
                     <div class="mb-3">
                         <label  class="form-label">Price</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1" />
+                        <input type="text" name='price' class="form-control" id="exampleInputPassword1" />
                     </div>
 
                     <button type="submit" class="btn btn-primary">Upload</button>
