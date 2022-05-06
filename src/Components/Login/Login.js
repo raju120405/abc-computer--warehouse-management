563e7e213b3a4d70c431d7f24cee5d8b22f8ecc1
raju120405 <raju120405@gmail.com>
2022-05-06 16:30:44 +0600

    redairection

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import auth from '../../Firebase/Firebase.init';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const [googleuser, googleloading, googleerror] = useAuthState(auth);
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
-    const hanleSignIn = ()=>{
+    let navigate = useNavigate();
+    let location = useLocation();
+   
+  
+    let from = location.state?.from?.pathname || "/";
+
+    const hanleSignIn = () => {
         signInWithGoogle()
+        
+            if(user)
+            {
+                navigate(from, { replace: true });
+            }
     }
     return (
         <div>
             <h2>this is login pages</h2>
-           <div className='text-center '> <button onClick={hanleSignIn} type="button" class="btn btn-success">Google Sign In</button></div>
+            <div className='text-center '> <button onClick={hanleSignIn} type="button" class="btn btn-success">Google Sign In</button></div>
         </div>
     );
 };
-    
+
 export default Login;
--- src/Components/UpdateProduct/UpdateProduct.js
+++ src/Components/UpdateProduct/UpdateProduct.js
@@ -3,7 +3,22 @@ import React from 'react';
 const UpdateProduct = () => {
     return (
         <div>
-            <h2>this is update products section</h2>
+            <h2 className='text-center mt-3 text-primary'>Upload your Product</h2>
+            <div className='w-50 mx-auto'>
+                <form>
+                    <div class="mb-3 ">
+                        <label  class="form-label">Product name</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        
+                    </div>
+                    <div class="mb-3">
+                        <label  class="form-label">Price</label>
+                        <input type="text" class="form-control" id="exampleInputPassword1" />
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Upload</button>
+                </form>
+            </div>
         </div>
     );
 };
