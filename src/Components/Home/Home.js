167817bfef909269f7d6f0a6845cb9e1734fc432
raju120405 <raju120405@gmail.com>
2022-05-08 00:22:07 +0600

    card

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
-        <div>
-            <h2 className='text-primary text-center mt-3'>Welcome To ABC Computers</h2>
+        <div className='container'>
+            <h2 className='text-primary text-center m-3'>Welcome To ABC Computers</h2>
+           
+            <div className='row container text-center '>
+                {
+                    products.map(pd => (<div className='col-4 '>
+
+                        <div class="card ">
+                            {/* <img src="..." class="card-img-top" alt="..."> */}
+                            <img src={pd.picture} alt="" />
+                            <div class="card-body">
+                                <h5 class="card-title"> Name: {pd.name}</h5>
+                                <h4>Price: {pd.price}</h4>
+                                <h6>Quantity: {pd.Quantity}</h6>
+                                <h6>Supplier: {pd.Supplier}</h6>
+                                <p>description: {pd.description}</p>
+
+                                <a href="#" class="btn btn-primary">Delivered</a>
+                            </div>
+                        </div>
+                    </div>))
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -2,17 +2,34 @@ import React, { useEffect, useState } from 'react';
 
 const Products = () => {
     const [products, setProducts] = useState([])
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:5000/products')
-        .then(res=>res.json())
-        .then(data=>setProducts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
-        <div>
-           <h2>total products:{products.length}</h2> 
-           <div className='row'>
-               
-           </div>
+        <div className='container'>
+            <h2 className='text-center m-4'>Total Products: {products.length}</h2>
+            <div className='row container text-center'>
+                {
+                    products.map(pd => (<div className='col-4 '>
+
+                        <div class="card text-center">
+                            {/* <img src="..." class="card-img-top" alt="..."> */}
+                            <img src={pd.picture} alt="" />
+                            <div class="card-body">
+                                <h5 class="card-title"> Name: {pd.name}</h5>
+                                <h4>Price: {pd.price}</h4>
+                                <h6>Quantity: {pd.Quantity}</h6>
+                                <h6>Supplier: {pd.Supplier}</h6>
+                                <p>description: {pd.description}</p>
+
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>))
+                }
+            </div>
         </div>
     );
 };
